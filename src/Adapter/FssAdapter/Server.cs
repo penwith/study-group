5e1edbbf6bc447c1e4c18a9974c83a097af7d8be
James Thomas <james.thomas@ukho.gov.uk>
2023-06-19 11:24:00 +0100

    fss adapter

---
+++ src/Adapter/FssAdapter/Server.cs
@@ -0,0 +1,27 @@
+﻿using Contracts;
+using FileShareService;
+
+namespace FssAdapter
+{
+    public class Server : IFtpServer
+    {
+        private readonly Service _fileShareService = new Service();
+
+        public void CWD(string workingDirectory)
+        {
+            var pathElements = workingDirectory.Split(@"\");
+            var week =int.Parse(pathElements[3]);
+            _fileShareService.SetWeek(week);
+        }
+
+        public void LIST()
+        {
+            _fileShareService.ListAllFiles();
+        }
+
+        public void RETR(string file)
+        {
+            _fileShareService.GetFile(file);
+        }
+    }
+}
