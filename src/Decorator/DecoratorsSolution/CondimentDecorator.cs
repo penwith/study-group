1a48b5bae1be363ebefe46125ef838cb8f0f5fe6
Jim <51706024+penwith@users.noreply.github.com>
2023-07-17 16:37:11 +0100

    Delete src/Decorator/DecoratorsSolution directory

--- src/Decorator/DecoratorsSolution/CondimentDecorator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Decorators
-{
-    public abstract class CondimentDecorator : Beverage
-    {
-        protected Beverage _beverage;
-
-        public abstract override string Description { get; }
-    }
-}
